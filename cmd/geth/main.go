bc6b325747b230b5d851da901a926f4831da5983
Simon Kim <skim104@illinois.edu>
2018-03-21 16:46:26 -0500

    maxnumfile option

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -52,6 +52,7 @@ var (
 	app = utils.NewApp(gitCommit, "the go-ethereum command line interface")
 	// flags that configure the node
 	nodeFlags = []cli.Flag{
+		utils.MaxNumFileFlag,
 		utils.MaxAcceptConnsFlag,
 		utils.IdentityFlag,
 		utils.UnlockedAccountFlag,
